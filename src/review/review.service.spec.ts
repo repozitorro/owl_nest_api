da8af9683716dbc36be8a5a7c6ff4d6f20401b60
repozitorro <repozitorro@gmail.com>
2022-04-03 17:15:22 +0300

    Add: review unit test

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewService } from './review.service';
+import { getModelToken } from 'nestjs-typegoose';
+import { mongoose } from '@typegoose/typegoose';
 
 describe('ReviewService', () => {
   let service: ReviewService;
+  const exec = { exec: jest.fn() };
+  const reviewRepositoryFactory = () => ({
+    find: () => exec,
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewService],
+      providers: [
+        ReviewService,
+        {
+          useFactory: reviewRepositoryFactory,
+          provide: getModelToken('ReviewModel'),
+        },
+      ],
     }).compile();
 
     service = module.get<ReviewService>(ReviewService);
@@ -14,5 +26,14 @@ describe('ReviewService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('find by product id working', async () => {
+    const id = new mongoose.Types.ObjectId().toHexString();
+    reviewRepositoryFactory()
+      .find()
+      .exec.mockReturnValueOnce([{ productId: id }]);
+    const res = await service.findByProductId(id);
+    expect(res[0].productId).toBe(id);
   });
 });
